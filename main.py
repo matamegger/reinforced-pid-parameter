98dfac47e5c7eaf0465bc893d5e83d8bb1910022
Matthias Tamegger <matamegger@gmail.com>
2021-11-29 20:02:47 +0100

    Add main for a basic playground for RL on the PID controlled system

---
+++ main.py
@@ -0,0 +1,54 @@
+import math
+import os.path
+import sys
+from typing import Callable
+
+from stable_baselines3 import A2C
+from stable_baselines3.common.vec_env import SubprocVecEnv
+
+from PidTunerEnv import PidTunerEnv
+
+
+def step(time):
+    if time >= 25.0:
+        return 1.0
+    else:
+        return 0.0
+
+
+def sin(time):
+    return math.sin(2 * math.pi * time/12)
+
+
+if __name__ == '__main__':
+    training = False
+    input_function = step
+    model_name = "PID-Parameter-Model"
+
+    # Basic argument  processing
+    if "sin" in sys.argv:
+        input_function = sin
+    elif "step" in sys.argv:
+        input_function = step
+    if "-t" in sys.argv:
+        training = True
+
+    # Setup
+    env = PidTunerEnv(input_function=input_function)
+    if os.path.isfile("PID-Parameter-Model.zip"):
+        model = A2C.load(model_name, env=env)
+    else:
+        model = A2C('MlpPolicy', env, verbose=1)
+
+    if training:
+        model.learn(total_timesteps=25000)
+        model.save(model_name)
+
+    obs = env.reset()
+    action, _state = model.predict(obs, deterministic=True)
+    print(action)
+    obs, reward, done, info = env.step(action)
+    print(reward)
+    env.render()
+    if done:
+        obs = env.reset()
